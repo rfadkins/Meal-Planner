61256dff5a897b89c099938e502200a733adb692
Raymond Adkins <rfadkins@hotmail.com>
2022-09-07 16:22:11 -0400

    IngredientController and RecipeController up and talking. Database SQL file updated to insert primary keys into dummy test data.

--- java/src/main/java/com/techelevator/controller/IngredientController.java
+++ java/src/main/java/com/techelevator/controller/IngredientController.java
@@ -3,19 +3,42 @@ package com.techelevator.controller;
 import com.techelevator.business.IngredientService;
 import com.techelevator.business.UserService;
 import com.techelevator.model.Ingredient;
+import com.techelevator.model.Recipe;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
-public class IngredientController {
+@RequestMapping("/ingredient")
+//@PreAuthorize("isAuthenticated()")
+public class IngredientController extends IngredientService{
     //This controller handles the Indredient table
+
+    UserService userService;
+    IngredientService ingredientService;
+
+    @Autowired
+    public IngredientController(UserService userService, IngredientService ingredientService) {
+        this.userService = userService;
+        this.ingredientService = ingredientService;
+    }
+
 
     /*--------------------
     createIngredient()
     POST
     PATH: /ingredient/
     --------------------*/
+    @PostMapping ("/")
+    public void createIngredient(@RequestBody Ingredient ingredient) {
+        ingredientService.createIngredient(ingredient.getIngredientName(),ingredient.getIngredientCategory());
+    }
 
 
     /*--------------------
@@ -23,6 +46,10 @@ public class IngredientController {
     PUT
     PATH: /ingredient/{ingredient_id}
     --------------------*/
+    @PutMapping ("/{ingredientId}")
+    public String editIngredient(@PathVariable("ingredientId") Long ingredientId, @RequestBody Ingredient ingredient) {
+        return ("Put/edit requests not working yet. However, I acknowledge to have received a put request for /ingredient/" + ingredientId);
+    }
 
 
     /*--------------------
@@ -30,20 +57,33 @@ public class IngredientController {
     DELETE
     PATH: /ingredient/{ingredient_id}
     --------------------*/
-
+    @DeleteMapping ("/{ingredientId}")
+    public void deleteIngredient(@PathVariable("ingredientId") Long ingredientId) {
+        ingredientService.deleteIngredient(ingredientId);
+    }
 
     /*--------------------
     getIngredient()
     GET
     PATH: /Ingredient/{Ingredient_id}
     --------------------*/
-
+    @GetMapping("/{ingredientId}")
+    public Ingredient getIngredient(@PathVariable("ingredientId") Long ingredientId) {
+        return ingredientService.getIngredient(ingredientId);
+    }
 
     /*--------------------
     List listIngredients()
     GET
     PATH: /Ingredient/
     --------------------*/
+    @GetMapping("/")
+    public List<Ingredient> listIngredients() {
+        return ingredientService.getAllIngredients();
+    }
+
+
+    //Friendly reminder that a user's ingredient list is their Pantry, and is handled in userOwnershipController
 
 
 
