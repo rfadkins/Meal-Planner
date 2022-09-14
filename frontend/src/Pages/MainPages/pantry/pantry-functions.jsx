43c175ef75b5183b862837efc8579c05da03980f
Paige <paige0011@gmail.com>
2022-09-14 15:35:47 -0500

    function skeleton

---
+++ frontend/src/Pages/MainPages/pantry/pantry-functions.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import axios from "axios";
+
+/* File contains functional code for pantry */
+
+const getAllPantryIngredients = async (e) => {
+  e.preventDefault();
+};
+
+const getIngredientsByCategory = async (e) => {
+    e.preventDefault();
+}
+
+const getIngredientsByName = async (e) => {
+    e.preventDefault();
+}
+
+const getPantryIngredient = async (e) => {
+    e.preventDefault();
+}
+
+const editPantryIngredient = async (e) => {
+    e.preventDefault();
+  };
+
+const deletePantryIngredient = async (e) => {
+    e.preventDefault();
+}
+
+const addNewIngredient = async (e) => {
+    e.preventDefault();
+
+    //have a drop down that is based of what is being typed, if there are no ingredients found, add new
+}
---
+++ frontend/src/Pages/MainPages/recipe-card/recipe-card-functions.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import axios from "axios";
+
+/* File contains functional code for recipe-card */
+
+const getAllUserRecipes = async (e) => {
+  e.preventDefault();
+};
+
+const getRecipesByCategory = async (e) => {
+    e.preventDefault();
+}
+
+const getRecipesByName = async (e) => {
+    e.preventDefault();
+}
+
+const getUserRecipe = async (e) => {
+    e.preventDefault();
+}
+
+const editUserRecipe = async (e) => {
+    e.preventDefault();
+  };
+
+const deleteUserRecipe = async (e) => {
+    e.preventDefault();
+}
+
+const addNewRecipe = async (e) => {
+    e.preventDefault();
+}
