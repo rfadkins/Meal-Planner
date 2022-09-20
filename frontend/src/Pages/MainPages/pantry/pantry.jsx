b5c29a680d3940c4831607c422c3c0e08ecc5429
Paige <paige0011@gmail.com>
2022-09-20 14:06:16 -0500

    added api integration for POST ingredients and GET pantry

--- frontend/src/Pages/MainPages/pantry/pantry.jsx
+++ frontend/src/Pages/MainPages/pantry/pantry.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import { Link, Routes, Route } from 'react-router-dom';
 
 import AddItemButtom from "../../../Components/Button.Components/ButtonAddItem/ButtonAddItem";
@@ -6,42 +6,60 @@ import ButtonSelectItem from "../../../Components/Button.Components/ButtonSelect
 import Bar from "../../../Components/Input.Component/search_bar/search_bar";
 import './pantry.css'
 
-import { getAllPantryIngredients, getAllIngredients } from "./pantry-functions";
+import { getAllPantryIngredients, getAllIngredients, addNewIngredient, testIngredientList } from "./pantry-functions";
 
 /*redux*/
 import { useSelector } from 'react-redux';
+import { useEffect } from "react";
 
-export default function Pantry(){
+export default function Pantry() {
     const currentUserId = useSelector((state) => state.user.id)
     const currentUserToken = useSelector((state) => state.token.token)
-    const [userPantry, setUserPantry] = useState(getAllPantryIngredients(currentUserId));
-
-    const allIngredients = getAllIngredients(currentUserToken)
-
+    const [userPantry, setUserPantry] = useState(null);
+    //const [userPantry, setUserPantry] = useState(testIngredientList);
+    
     //add new ingredients
     const [newIngredientName, setNewIngredientName] = useState("")
     const [newIngredientCategory, setNewIngredientCategory] = useState("")
-    const [newIngredientCount, setNewIngredientCount] = useState("")
+    const [newIngredient, setNewIngredient] = useState(false)
 
+    useEffect(() => {
+        getAllPantryIngredients(currentUserId, currentUserToken)
+        .then(function(result){setUserPantry(result)})
+    }, [])
 
-    return(
+    function addIngredientOnClick() {
+        //format and POST new ingredient
+        const ingredient = { ingredientName: newIngredientName, ingredientCategory: newIngredientCategory }
+        addNewIngredient(currentUserId, currentUserToken, ingredient)
+        //reset input values
+        setNewIngredientName("")
+        setNewIngredientCategory("")
+        //refresh pantry
+
+        //setNewIngredient(newIngredient ? false : true)
+        //console.log(newIngredient)
+    }
+
+    return (
         <div>
-            <Bar/>
-        <div className="Pantry-body">
-            
-            <AddItemButtom buttonImage='+' nameHandle="Pantry-add">
-                <h3 className="title">Add ingredient</h3>
-                <p>Item name? <input type="text" className="textInputLong"/></p>
-                <p>Catagory? <input type="text" className="textInputLong"/></p>
-                <div className="countAndDate">
-                    <p className="E">How many? <input type="text" className="textInputShort"/></p>
-                </div>
-                <button className="submitButton">submit</button>
-            </AddItemButtom>
-            
-            <ButtonSelectItem items={userPantry}/>
-            
-        </div>
+            <Bar />
+            <div className="Pantry-body">
+
+                <AddItemButtom buttonImage='+' nameHandle="Pantry-add">
+                    <h3 className="title">Add ingredient</h3>
+                    <p>Ingredient Name <input type="text" className="textInputLong" value={newIngredientName} onChange={() => setNewIngredientName(event.target.value)} /></p>
+                    <p>Category <input type="text" className="textInputLong" value={newIngredientCategory} onChange={() => setNewIngredientCategory(event.target.value)} /></p>
+                    <button className="submitButton" onClick={addIngredientOnClick}>submit</button>
+                </AddItemButtom>
+
+                <ButtonSelectItem items={userPantry} />
+
+            </div>
         </div>
     )
 }
+
+//upgrades
+//pull existing categories as options in the dropdown
+//have categories be created and maintained in a settings menu
