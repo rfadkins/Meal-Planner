c2bb76863c3956644a7587c0af9c02883a59551a
Paige <paige0011@gmail.com>
2022-09-23 06:49:12 -0500

    Updated redux states

--- frontend/src/Redux/configureStore.jsx
+++ frontend/src/Redux/configureStore.jsx
@@ -1,12 +1,14 @@
 import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import tokenReducer from "./tokenSlice";
+import dashboardReducer from "./dashboardSlice"
 import thunk from "redux-thunk";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     token: tokenReducer,
+    dashboard: dashboardReducer
   },
 });
 
