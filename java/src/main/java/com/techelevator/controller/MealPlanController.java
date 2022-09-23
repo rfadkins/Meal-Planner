c25debbdd9ef50a4ae9edf585557c9172c66c9c5
Raymond Adkins <rfadkins@hotmail.com>
2022-09-22 20:50:25 -0400

    meal plan error handling implemented and tested

--- java/src/main/java/com/techelevator/controller/MealPlanController.java
+++ java/src/main/java/com/techelevator/controller/MealPlanController.java
@@ -3,18 +3,23 @@ package com.techelevator.controller;
 import com.techelevator.business.MealPlanService;
 import com.techelevator.business.UserService;
 
+import com.techelevator.exceptions.IngredientNotFoundException;
+import com.techelevator.exceptions.MealNotFoundException;
+import com.techelevator.exceptions.MealPlanNotFoundException;
+import com.techelevator.model.Ingredient;
 import com.techelevator.model.MealPlan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 
 @RestController
 @CrossOrigin
-@PreAuthorize("isAuthenticated()")
+//@PreAuthorize("isAuthenticated()")
 public class MealPlanController {
     //This controller handles the meal_plan table
 
@@ -41,7 +46,15 @@ public class MealPlanController {
 
     @GetMapping ("/mealplan/{mealplanId}")
     public MealPlan displayMealPlan(@PathVariable("mealplanId") Long mealplanID) {
-        return mealPlanService.displayMealPlan(mealplanID);
+        try {
+            MealPlan mealPlan = mealPlanService.displayMealPlan(mealplanID);
+            if (mealPlan == null) {
+                throw new MealPlanNotFoundException();
+            }
+            return mealPlanService.displayMealPlan(mealplanID);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Meal Plan not found", e);
+        }
     }
 
 
@@ -54,13 +67,35 @@ public class MealPlanController {
     @PutMapping ("/mealplan/{mealplanId}")
     public MealPlan editMealPlan(@PathVariable("mealplanId") Long mealplanID,
                                 @RequestBody MealPlan mealPlan) {
-        return this.mealPlanService.editMealPlan(mealplanID, mealPlan.getMealPlanName());
+        try {
+            MealPlan editedMealPlan = mealPlanService.displayMealPlan(mealplanID);
+            if (editedMealPlan == null) {
+                throw new IngredientNotFoundException();
+            } else if (mealPlan == null) {
+                throw new IngredientNotFoundException();
+            } else {
+                return this.mealPlanService.editMealPlan(mealplanID, mealPlan.getMealPlanName());
+            }
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Meal Plan not found", e);
+        }
+
     }
 
 
     @DeleteMapping ("/mealplan/{mealplanId}")
-    public String editMealPlan(@PathVariable("mealplanId") Long mealplanID) {
-        return this.mealPlanService.deleteMealPlan(mealplanID);
+    public void deleteMealPlan(@PathVariable("mealplanId") Long mealplanId) {
+        //return this.mealPlanService.deleteMealPlan(mealplanID);
+        try {
+            MealPlan deletedMealPlan = mealPlanService.displayMealPlan(mealplanId);
+            if (deletedMealPlan == null) {
+                throw new MealNotFoundException();
+            } else {
+                mealPlanService.deleteMealPlan(mealplanId);
+            }
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Meal Plan not found", e);
+        }
     }
 
 
